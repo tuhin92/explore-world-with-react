8f17f19240673af926abfc34ac448225ca3c41be
tuhin92 <tuhinalmamun71m@gmail.com>
2024-03-12 11:39:26 +0600

    Conditional CSS

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Country.css';
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountry}) => {
     const {name, flags, population, area, cca3} = country;
 
     const [visited, setVisited] = useState(false);
@@ -8,13 +8,16 @@ const Country = ({country}) => {
         setVisited(!visited);
     }
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'visited'}`}>
             <h3>Name: {name?.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>Area: {area} sqr Km.</p>
             <p><small>Code: {cca3}</small></p>
+            <button>Mark Visited</button>
+            <br /><br />
             <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
+            <br />
             {visited ? 'I already visited this country.' : 'I want to visit this country'}
         </div>
     );
--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -6,18 +6,34 @@ import './Countrise.css'
 const Countries = () => {
     const [countries, setCountries] = useState([]);
 
+    const [visitedCountries, setVisitedCountries] = useState([]);
+
     useEffect (()=>{
         fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then(data => setCountries(data));
     } ,[])
+
+    const handleVisitedCountry = country =>{
+        console.log('add this to your country');
+        console.log(country);
+    }
     return (
         <div>
             <h3>Countries: {countries.length}</h3>
+
+            <div>
+                <h5>Visited Countries</h5>
+                <ul>
+
+                </ul>
+            </div>
+
             <div className="country-container">
                {
                 countries.map(country => <Country 
                     key={country.cca3} 
+                    handleVisitedCountry = {handleVisitedCountry}
                     country={country}></Country>)
                }
             </div>            
